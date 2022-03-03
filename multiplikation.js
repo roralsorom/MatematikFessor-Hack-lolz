b9166e035fc60f667376bf9a541c88341bc61223
roralsorom <86061161+roralsorom@users.noreply.github.com>
2022-03-03 12:27:37 +0100

    Update multiplikation.js

--- multiplikation.js
+++ multiplikation.js
@@ -1,24 +1,7 @@
-try {
-    console.clear()
-    console.log('%c [+] ', 'background: #222; color: #00FFFF', 'Emat Hack loaded');
-    console.log("Searching for question");
-    var question1 = document.getElementsByClassName('mjx-char MJXc-TeX-main-R')[0].innerText;
-    var sss = document.getElementsByClassName('mjx-char MJXc-TeX-main-R').innerText;
-    var question2 = document.getElementsByClassName('mjx-char MJXc-TeX-main-R')[2].innerText;
-    var ss = document.getElementsByClassName('mjx-char MJXc-TeX-main-R')[3].innerText;
-    var question3 = document.getElementsByClassName('mjx-char MJXc-TeX-main-R')[4].innerText;
-    console.log("Found Question: " + question1, question2, question3);
-    anssss = question1 * question2 * question3;
-    console.log("Answer: "  + anssss);
-} catch (err) {
-    ansssssssss = question1 * question2;
-    console.log("Answer: "  + ansssssssss);
-}
-
 function lolzzz() {
     try {
         console.clear()
-        console.log('%c [+] ', 'background: #222; color: #00FFFF', 'Emat Hack loaded');
+        console.log('%c [+] ', 'background: #222; color: #00FFFF', 'Matematikfessor Multiplikation Hack);
         console.log("Searching for question");
         var question1 = document.getElementsByClassName('mjx-char MJXc-TeX-main-R')[0].innerText;
         var sss = document.getElementsByClassName('mjx-char MJXc-TeX-main-R').innerText;
@@ -36,3 +19,4 @@ function lolzzz() {
 }
 
 setInterval(lolzzz, 1000)
+//MateMatikfessor Multiplikation hack
